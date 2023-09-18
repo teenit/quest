3f42412c97ba57c51402c88b23d7ed19ff2dcdec
Pavlo <teenitclub@gmail.com>
2023-09-18 18:50:14 +0300

    First setting and first start

--- front/src/index.js
+++ front/src/index.js
@@ -4,12 +4,6 @@ import App from './components/app';
 import './main.scss';
 import './fonts/fonts.scss';
 
-function component(text) {
-  const element = document.createElement('h1');
-  element.textContent = text;
-  return element;
-}
 
-document.body.prepend(component('Проект собран на Webpack'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
