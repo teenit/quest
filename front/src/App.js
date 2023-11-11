dcbf84416c708feadd1f90489b231e2597e62482
Pavlo <teenitclub@gmail.com>
2023-11-11 14:06:53 +0200

    settings new

---
+++ front/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Мой топ фото</h1>
+        </header>
+        <p className="App-intro">
+          Здесь будут мои самые залайканые фото
+        </p>
+        <p>Меня зовут: {this.props.user}</p>{' '}
+        {/* добавлен вывод из props */}
+      </div>
+    );
+  }
+}
+
+// приклеиваем данные из store
+const mapStateToProps = (store) => {
+  console.log(store); // посмотрим, что же у нас в store?
+  return {
+    user: store.user,
+  };
+};
+
+// в наш компонент App, с помощью connect(mapStateToProps)
+export default connect(mapStateToProps)(App);
--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './App';
 import './main.scss';
+
 import './fonts/fonts.scss';
+import { Provider } from 'react-redux';
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Provider><App tab="home" /></Provider>);
