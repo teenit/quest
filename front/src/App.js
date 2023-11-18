f7770567f38d6c41fbcf72d221b164e467cc4443
Pavlo <teenitclub@gmail.com>
2023-11-18 12:24:18 +0200

    Setting2

--- front/src/App.js
+++ front/src/App.js
@@ -6,13 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='open'>5555555</div>
         <header className="App-header">
+        <div className='open'>5555555</div>
           <h1 className="App-title">Мой топ фото</h1>
+          <p className="App-intro">
+             Здесь будут мои самые залайканые фото
+          </p>
+          <p className="App-intro active">
+          Здесь будут мои самые залайканые фото
+        </p>
         </header>
         <p className="App-intro">
           Здесь будут мои самые залайканые фото
         </p>
         <p>Меня зовут: {this.props.user}</p>{' '}
+        <div className='open'>5555555</div>
         {/* добавлен вывод из props */}
       </div>
     );
--- front/src/index.js
+++ front/src/index.js
@@ -7,6 +7,12 @@ import './fonts/fonts.scss';
 import { Provider } from 'react-redux';
 
 import { createRoot } from 'react-dom/client';
+import { store } from './store/configureStore';
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<Provider><App tab="home" /></Provider>);
+const root = createRoot(container);
+
+root.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+);
